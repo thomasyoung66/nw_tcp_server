eee690ca30f0020c8617d20fbbcfa4cf23155f1e
thomasyoung66 <65619882@qq.com>
2017-07-04 00:16:22 -0700

    add ini parser

---
+++ src/xx_game/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ * 
+ *        Filename:  MAIN
+ * 
+ *     Description:  
+ * 
+ *         Version:  1.0
+ *         Created:  07/03/2017 11:50:10 PM PDT
+ *        Revision:  none
+ *        Compiler:  gcc
+ * 
+ *          Author:  YangLiuShun (Thomas Young), 65619882@qq.com
+ *         Company:  no name company
+ * =====================================================================================
+ */
+
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc,char * argv[])
+{
+	return 0;
+}
+
+#endif  
+
