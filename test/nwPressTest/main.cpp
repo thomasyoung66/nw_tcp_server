a66e82b670772f32c01cf63028ad3d726ff084f2
thomasyoung66 <65619882@qq.com>
2017-07-05 19:03:46 -0700

    add test tool

---
+++ test/nwPressTest/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ * 
+ *        Filename:  MAIN
+ * 
+ *     Description:  
+ * 
+ *         Version:  1.0
+ *         Created:  07/05/2017 07:03:09 PM PDT
+ *        Revision:  none
+ *        Compiler:  gcc
+ * 
+ *          Author:  YangLiuShun (Thomas Young), 65619882@qq.com
+ *         Company:  no name company
+ * =====================================================================================
+ */
+
+#ifndef _MAIN_
+#define _MAIN_
+
+
+int main(int argc,char * argv[])
+{
+	return 0;
+}
+
+
+
+#endif  
+
